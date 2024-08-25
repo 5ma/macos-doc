040b058ea733e2aa5720a37a865beea630cbf1f0
5ma <bbxxuw@gmail.com>
2024-08-25 18:14:03 +0900

    Update HTML and Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const pxToRem = (px, defaultFontSize = 16) => {
+  return `${px / defaultFontSize}rem`
+}
+
 module.exports = {
   mode: 'jit',
   content: ["./src/*.html", "./src/*.{js,jsx,ts,tsx,vue}"],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
+    spacing: Object.fromEntries(Array.from({ length: 300 }, (_, i) => [`${i}`, pxToRem(i)])),
+    fontSize: Object.fromEntries(Array.from({ length: 200 }, (_, i) => [`${i}`, pxToRem(i)])),
     extend: {},
   },
   variants: {
